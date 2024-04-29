5b9e8a3ad501896bd5b4110da7d6b7e3dfbde0b9
trickster1o1 <nischaltuladhar15@gmail.com>
2024-04-29 17:17:32 +0545

    featured component section fix

--- src/components/featured.jsx
+++ src/components/featured.jsx
@@ -2,8 +2,8 @@ export default function Featured() {
   return (
     <div className="feature-cont">
         <span className="sub-topic">Honorable Mentions</span>
-        <h1 className="custom-header">Featured Project</h1>
-        <span>Some of the projects Business Name repaired</span>
+        <h1 className="custom-header">Featured Projects</h1>
+        <span>Some of the projects Business Name repaired.</span>
       <div className="f-info">
         <div className="f-overlay"></div>
         <div>
