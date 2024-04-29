3c82745df1a28559ab35a86e6e2854f622b823d4
trickster1o1 <nischaltuladhar15@gmail.com>
2024-04-29 12:49:55 +0545

    base init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./assets/main.scss";
+import Header from "./components/header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="cont">
+        <div>Working</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,30 @@
+import ig from "../assets/images/logo/ig.svg";
+import lin from "../assets/images/logo/in.svg";
+import whatsapp from "../assets/images/logo/whatsapp.svg";
+export default function Header() {
+  return (
+    <>
+      <div className="top-header">
+        <div>
+          <span>Mon-Fri: 9AM - 5pm</span>
+          <span>yourbusiness@business.com</span>
+        </div>
+        <div>
+          <img src={ig} alt="IG" />
+          <img src={lin} alt="IG" />
+          <img src={whatsapp} alt="IG" />
+        </div>
+      </div>
+      <nav className="navbar">
+        <div>Business Logo</div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Serices</li>
+            <li>Contact</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
