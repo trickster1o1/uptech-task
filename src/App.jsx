41563d4a752dec4397c4c9d8bef93d296c618bc9
trickster1o1 <nischaltuladhar15@gmail.com>
2024-05-02 00:06:20 +0545

    splash screen init

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import "./assets/main.scss";
 import About from "./components/about";
 import Contact from "./components/contact";
@@ -8,8 +9,17 @@ import Service from "./components/services";
 import ClientStory from "./components/story";
 
 function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      document.getElementById('splash').style.opacity = '0';
+      document.getElementById('splash').style.zIndex = '-1';
+    }, 2500);
+  }, []);
   return (
     <>
+    <div className="splash" id="splash">
+      <h1 className="custom-header">Business Logo</h1>
+    </div>
       <Header />
        <About />
       <Service />
