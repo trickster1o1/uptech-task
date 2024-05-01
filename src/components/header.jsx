e4f24887de8d052da63a8bef2a5c66e6f0241266
trickster1o1 <nischaltuladhar15@gmail.com>
2024-05-01 23:49:38 +0545

    mobile nav init

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import ig from "../assets/images/logo/ig.svg";
 import lin from "../assets/images/logo/in.svg";
 import whatsapp from "../assets/images/logo/whatsapp.svg";
 import HeroSection from "./hero";
 export default function Header() {
+  const [showNav,setShowNav] = useState(false);
   return (
     <>
+      <div className="mob-nav" style={showNav ? {'display':'flex'}: {'display':'none'}}>
+        <span class="material-symbols-outlined cl-nav" onClick={()=>setShowNav(false)}>close</span>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Projects</a>
+          </li>
+          <li>
+            <a href="/">Serices</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+      </div>
       <div className="top-header">
         <div>
           <span>Mon-Fri: 9AM - 5pm</span>
@@ -25,7 +47,7 @@ export default function Header() {
           <li>Serices</li>
           <li>Contact</li>
         </ul>
-        <span class="material-symbols-outlined b-mnu">menu</span>
+        <span class="material-symbols-outlined b-mnu" onClick={()=>setShowNav(true)}>menu</span>
       </nav>
       <HeroSection />
     </>
